8865ff89d4b35a5686ba2fad7a654e79f04dc858
Luke Chi <luke.chi@gmail.com>
2015-07-09 11:36:55 +0800

    add files

---
+++ web/main.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+import 'package:color_run_tw/nav_menu.dart';
+import 'package:color_run_tw/reverser.dart';
+import 'package:route_hierarchical/client.dart';
+
+void main() {
+  initNavMenu();
+  initReverser();
+
+  // Webapps need routing to listen for changes to the URL.
+  var router = new Router();
+  router.root
+    ..addRoute(name: 'about', path: '/about', enter: showAbout)
+    ..addRoute(name: 'home', defaultRoute: true, path: '/', enter: showHome);
+  router.listen();
+}
+
+void showAbout(RouteEvent e) {
+  // Extremely simple and non-scalable way to show different views.
+  querySelector('#home').style.display = 'none';
+  querySelector('#about').style.display = '';
+}
+
+void showHome(RouteEvent e) {
+  querySelector('#home').style.display = '';
+  querySelector('#about').style.display = 'none';
+}
